b83353ffd7fd7f6affeb14fc77ae50bfc30b43e8
NissiChandana <nissi.neerudu@gmail.com>
2023-12-06 19:45:10 -0500

    commit

--- server.js
+++ server.js
@@ -22,10 +22,14 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 })
 
 const  responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
-const magic8BallResponses = responses[Math.floor(Math.random()*responses.length)]
+
+
+
 app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res) => {
+    let magic8BallResponses = responses[Math.floor(Math.random()*responses.length)]
     console.log(req.params)
-    
+    console.log(magic8BallResponses);
+    console.log(Math.floor(Math.random()*responses.length))
     res.send( `<h3> Will I be a millionaire ? </h3>
     <h1>${magic8BallResponses}</h1>`)
  
@@ -34,7 +38,7 @@ app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res) => {
 
 
 app.get('/', (req, res) => {
-    res.send('<h1> This is the root</h1>')
+    res.send('<h1> Hello This is the root</h1>')
 })
 
 
